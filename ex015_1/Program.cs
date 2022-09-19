9a6636a8d4eaf843fb0ef77e899f5f3e0e966dc9
lowrider48rus <lowrider48rus@gmail.com>
2022-09-19 22:24:25 +0300

    ex015 - выяснение введенной цифры дня недели на соответствие выходному дню. Второй способ

---
+++ ex015_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//вариант 2 - попроще
+
+Console.WriteLine("Type number of day (between 1 to 7): ");
+int day = int.Parse(Console.ReadLine());
+
+if ((day >=6) && (day <= 7)){
+    Console.WriteLine($"day of the week mumber {day} IS the day off, relax ;)");
+}
+
+else{
+    Console.WriteLine($"WTF man, you type number {day} which IS NOT the day off!!!");
+}
